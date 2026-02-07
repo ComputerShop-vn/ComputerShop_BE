3ee6203f3780e9aec5da27d5b2bbf321959fc156
Tri <quachtri865@gmail.com>
2026-02-07 12:09:50 +0700

    feat: new NotFoundException

---
+++ src/main/java/sp26/group3/computer/sba301_computershop/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package sp26.group3.computer.sba301_computershop.exception;
+
+public class NotFoundException extends RuntimeException{
+    public  NotFoundException(String message) {
+        super(message);
+    }
+}
