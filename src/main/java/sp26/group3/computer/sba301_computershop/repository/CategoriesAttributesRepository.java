6023baacfccf8fc6921d9db3cc440234d9c10705
Tri <quachtri865@gmail.com>
2026-02-07 12:23:12 +0700

    feat: new class impl attribute service

---
+++ src/main/java/sp26/group3/computer/sba301_computershop/repository/CategoriesAttributesRepository.java
@@ -0,0 +1,7 @@
+package sp26.group3.computer.sba301_computershop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sp26.group3.computer.sba301_computershop.entity.CategoriesAttributes;
+
+public interface CategoriesAttributesRepository extends JpaRepository<CategoriesAttributes, Integer> {
+}
